559186b631c4d00e6c188d68f1df68870bddcb94
NicholasPufal <niko.pufal@gmail.com>
2012-09-15 18:39:56 -0300

    Modified a lot of stuff.

--- js/core.js
+++ js/core.js
@@ -1,39 +1,42 @@
-function genRandomText()
-{
-  this.numWordsParagraph = localStorage["numWordsParagraphs"]; //Numbers of words per paragraph 
-  this.numParagraphs = localStorage["numParagraphs"]; //Number of paragraphs
-  
-  this.dummyWords = ["integer","vut","nunc","risus","a","sagittis","turpis", "nunc","eu","urna","urna", "pellentesque","porttitor","est","ut","augue","cursus","scelerisque", "in","hac","habitasse","platea","dictumst", "sed","ut","odio","a","dolor","ultricies","dapibus", "cum","sociis","natoque","penatibus","et","magnis","dis","parturient","montes","nascetur","ridiculus","mus", "etiam","vel","lacus","magna","nec","aliquam","augue", "lundium", "integer","porttitor","porta","porta", "in","rhoncus","adipiscing","diam","sit","amet","ultrices","turpis","auctor","sit","amet", "aenean","pulvinar","egestas","lorem","ac","placerat", "sed","lectus","mauris","rhoncus","mid","tincidunt","dignissim","elementum","in","odio", "duis","vel","magna","elit", "phasellus","tincidunt","nisi","pid","pulvinar","placerat","purus","augue","aliquet","tortor","et","tristique","turpis","enim","nec","nisi", "proin","facilisis","adipiscing","enim","ac","mattis","arcu","elementum","et", "cras","massa","non","velit","tempor","scelerisque","ac","quis","eros"];
-  this.punctuation = ["!","?","."];
-  
-//-------------------
-  
-  this.getRandomWord = function (x)
-  {
-    return x[this.getRandomNumber(x.length)];
+genRandomText = function() {
+  var that = {
+    numWordsParagraph: localStorage["numWordsParagraphs"], //Numbers of words per paragraph
+    numParagraphs: localStorage["numParagraphs"], //Number of paragraphs
+    dummyWords: ["integer","vut","nunc","risus","a","sagittis","turpis", "nunc","eu","urna","urna", "pellentesque",
+                  "porttitor","est","ut","augue","cursus","scelerisque", "in","hac","habitasse","platea","dictumst",
+                  "sed","ut","odio","a","dolor","ultricies","dapibus", "cum","sociis","natoque","penatibus","et","magnis",
+                  "dis","parturient","montes","nascetur","ridiculus","mus", "etiam","vel","lacus","magna","nec","aliquam",
+                  "augue", "lundium", "integer","porttitor","porta","porta", "in","rhoncus","adipiscing","diam","sit","amet",
+                  "ultrices","turpis","auctor","sit","amet", "aenean","pulvinar","egestas","lorem","ac","placerat", "sed","lectus",
+                  "mauris","rhoncus","mid","tincidunt","dignissim","elementum","in","odio", "duis","vel","magna","elit", "phasellus",
+                  "tincidunt","nisi","pid","pulvinar","placerat","purus","augue","aliquet","tortor","et","tristique","turpis","enim",
+                  "nec","nisi", "proin","facilisis","adipiscing","enim","ac","mattis","arcu","elementum","et", "cras","massa","non",
+                  "velit","tempor","scelerisque","ac","quis","eros"],
+    punctuation: ["!","?","."]
   }
-  
-  this.getRandomNumber = function (x)
-  {
+
+  that.getRandomWord = function(x) {
+    return x[that.getRandomNumber(x.length)];
+  }
+
+  that.getRandomNumber = function (x) {
     return Math.floor(Math.random()*parseInt(x));
   }
 
-  this.upperFirst = function(x)
-  {
+  that.upperFirst = function(x) {
     var er = /[<p>|\?|\!|\.]\W([a-z])/g;
     var ex = x.replace(er,function (s) { return s.toUpperCase(); });
     return ex;
   }
 
-  this.getRandomPunctuation = function (x)
-  {
+  that.getRandomPunctuation = function (x) {
     if (x > 0)
     {
-      if (x % this.getRandomNumber(30) == 0)
+      if (x % that.getRandomNumber(30) == 0)
       {
-        return this.getRandomWord(this.punctuation) + ' ';
+        return that.getRandomWord(that.punctuation) + ' ';
       }
-      else if (x % this.getRandomNumber(20) == 0)
+      else if (x % that.getRandomNumber(20) == 0)
       {
         return ', ';
       }
@@ -44,14 +47,13 @@ function genRandomText()
     }
     return ' ';
   }
-  
-  this.compileText = function ()
-  {
+
+  that.compileText = function () {
     html = '<p>';
-    
-    for (var x=0,y=this.numWordsParagraph*this.numParagraphs;x<=y;x++) 
+
+    for (var x=0,y=that.numWordsParagraph*that.numParagraphs;x<=y;x++)
     {
-      if ((x > 0) && (x % this.numWordsParagraph == 0))
+      if ((x > 0) && (x % that.numWordsParagraph == 0))
       {
         html += '.</p>\n\n<p>';
       }
@@ -59,12 +61,14 @@ function genRandomText()
       {
         if (html.substring(html.length - 3, html.length) != "<p>")
         {
-          html += this.getRandomPunctuation(x);        
+          html += that.getRandomPunctuation(x);
         }
-        
-        html += this.getRandomWord(this.dummyWords);
+
+        html += that.getRandomWord(that.dummyWords);
       }
     }
-    return this.upperFirst(html.substring(0, html.length - 3));
+    return that.upperFirst(html.substring(0, html.length - 3));
   }
+
+  return that;
 }
